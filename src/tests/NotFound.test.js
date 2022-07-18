7defca06dab457615886ab51153830b72f371572
Victor Murilo <victor2706.vm@gmail.com>
2022-07-18 16:55:30 -0300

    Cumpri o requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 04', () => {
+  it('Verfica se a página contém um h2 com o texto "Page requested not found 😭"',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/nao-existe');
+
+      const h2NotFound = screen.getByText(/page requested not found/i);
+      expect(h2NotFound).toBeDefined();
+    });
+
+  it('Verifica se a página NotFound mostra a imagem do Pikachu chorando',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/nao-existe');
+
+      const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+      const imageNotFound = screen
+        .getByAltText(/Pikachu crying because the page requested was not found/i);
+      expect(imageNotFound.src).toBe(url);
+    });
+});
